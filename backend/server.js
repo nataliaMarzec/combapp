cda39565448cba41d8bc1cab6a9372d95e612d50
nataliaMarzec <natimarzec708@gmail.com>
2020-09-01 00:47:37 -0300

    agregado server

---
+++ backend/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+var Sequelize = require('sequelize');
+bodyParser = require("body-parser");
+
+var path = require("path");
+var debug = require('debug')('express-sequelize');
+
+const server= express();
+
+
+
+
+server.use(bodyParser.json());
+
+server.set('port',process.env.PORT ||8888);
+server.get("/", (req, res) => res.send('APP UP'));
+
+
+console.log("AQUI SERVER:",path.join(__dirname,`server`));
+
+// sequelize.sync({force:true})
+// sequelize.sync()
+// .then(() => {console.log(`--modelos sincronizados!!!--`)})
+// .then(() => {
+  server.listen(server.get('port'),()=> {
+  debug(`Express listening on port ${server.get('port')}`);
+  
+  });
+// });
+
+
+
+
+exports.server=server
